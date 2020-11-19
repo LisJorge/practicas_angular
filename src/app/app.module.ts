1bf7c1836b6d71223c70e89bf67dcb09761d9897
ricardo <ricardosudasassi@hotmail.com>
2020-11-19 16:17:02 -0600

    practica 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SidebarModule } from './sidebar/sidebar.module';
 import { AppComponent } from './app.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { Practica1Component } from './practica1/practica1.component';
 
 @NgModule({
   imports: [
@@ -26,7 +27,8 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    Practica1Component
   ],
   providers: [],
   bootstrap: [AppComponent]
